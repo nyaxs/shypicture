0441206f69f9b8637dbb889802e4c5a8d0cf5cf6
nyaxs <nyaxs@outlook.com>
2020-05-31 21:31:22 +0800

    尝试使用异步下载图片

---
+++ src/main/java/com/nyaxs/shypicture/util/TaskPoolConfig.java
@@ -0,0 +1,10 @@
+package com.nyaxs.shypicture.util;
+
+/**
+ * @author nyaxs
+ * @version V1.0
+ * @Package com.nyaxs.shypicture.util
+ * @date 2020/5/31 15:14
+ */
+public class TaskPoolConfig {
+}
